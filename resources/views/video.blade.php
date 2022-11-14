00305cae1b0c06f27da0fc6ea518c72b039931c9
Faris Ahmed <faris@ana.ae>
2022-11-15 01:12:32 +0200

    first commit

---
+++ resources/views/video.blade.php
@@ -0,0 +1,15 @@
+<video:video>
+    <video:thumbnail_loc>{{ $video->thumbnail_loc }}</video:thumbnail_loc>
+    <video:title>{{ $video->title }}</video:title>
+    <video:description>{{ $video->description }}</video:description>
+    <video:player_loc>{{ $video->player_loc }}</video:player_loc>
+
+    @if ($video->duration > 0)
+        <video:duration>{{ $video->duration }}</video:duration>
+    @endif
+
+    @if (! empty($video->expiration_date))
+        <video:expiration_date>{{ $video->expiration_date }}</video:expiration_date>
+    @endif
+
+</video:video>
